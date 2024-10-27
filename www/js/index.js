3e6951313bf8dc88712cca0d76d20e912d95eb9f
josmardiaz92 <josmardiaz92@gmail.com>
2024-10-27 17:00:21 -0400

    comenzamos a orientar a objetos el proyecto

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,50 @@
+const contenedorNotas=document.getElementById('contenedorNotas');
 const ModalNotas=document.getElementById('modal');
 const intanceModalNotas=new bootstrap.Modal(ModalNotas);
-const contenedorNotas=document.getElementById('contenedorNotas');
 
-const notas=obtenerNotas();;
+class Notas{
+    constructor(){
+        this.notas=this.obtenerNotas();
+    }
 
-function agregar(){
-    const tituloNotas=document.getElementById('tituloNotas').value;
-    const textoNotas=document.getElementById('textoNotas').value;
-    notas.push({titulo:tituloNotas,texto:textoNotas});
-    localStorage.setItem('notas',JSON.stringify(notas));
-    mostrarNotas();
-    intanceModalNotas.hide();
-}
-function mostrarNotas(){
-    contenedorNotas.innerHTML='';
-    notas.forEach(nota => {
-        contenedorNotas.innerHTML+=`
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${nota.titulo}</h5>
-                    <p class="card-text">${nota.texto}</p>
+    obtenerNotas(){
+        let notasGuardadas=JSON.parse(localStorage.getItem('notas'));
+        return notasGuardadas || []
+    }
+    mostrarNotas(){
+        contenedorNotas.innerHTML='';
+        this.notas.forEach(nota => {
+            contenedorNotas.innerHTML+=`
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${nota.titulo}</h5>
+                            <p class="card-text">${nota.texto}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    `
-    });
+            `
+        });
+    }
+    agregarNota(){
+        const titulo=document.getElementById('tituloNotas').value;
+        const texto=document.getElementById('textoNotas').value;
+        this.notas.push({titulo:titulo,texto:texto});
+        this.guardarNota();
+    }
+    guardarNota(){
+        let notasString=JSON.stringify(this.notas)
+        localStorage.setItem('notas',notasString);
+        this.mostrarNotas();
+        intanceModalNotas.hide();
+    }
+
 }
+
+const notas=new Notas();
+notas.mostrarNotas();
 
 ModalNotas.addEventListener('hidden.bs.modal',()=>{
     document.getElementById('tituloNotas').value='';
     document.getElementById('textoNotas').value='';
 })
-
-function obtenerNotas(){
-    let notasGuardadas=JSON.parse(localStorage.getItem('notas'));
-    if(notasGuardadas===null){
-        result=[]
-    }else{
-        result=notasGuardadas;
-    }
-    return result;
-}
-
-mostrarNotas()
