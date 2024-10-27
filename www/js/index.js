affd5d02bd8e9913dcffc13241dbea55156258c9
josmardiaz92 <josmardiaz92@gmail.com>
2024-10-27 18:29:48 -0400

    Clase 3 - buscar listo

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 const contenedorNotas=document.getElementById('contenedorNotas');
+const contenedorAnuncios=document.getElementById('contenedorAnuncios');
 const ModalNotas=document.getElementById('modal');
 const intanceModalNotas=new bootstrap.Modal(ModalNotas);
 
@@ -11,9 +12,9 @@ class Notas{
         let notasGuardadas=JSON.parse(localStorage.getItem('notas'));
         return notasGuardadas || []
     }
-    mostrarNotas(){
+    mostrarNotas(notasFiltradas){
         contenedorNotas.innerHTML='';
-        this.notas.forEach((nota,index) => {
+        notasFiltradas.forEach((nota,index) => {
             contenedorNotas.innerHTML+=`
                 <div class="col">
                     <div class="card" onclick="notas.seleccionarNota(${index})" id="${index}">
@@ -35,7 +36,7 @@ class Notas{
     guardarNota(){
         let notasString=JSON.stringify(this.notas)
         localStorage.setItem('notas',notasString);
-        this.mostrarNotas();
+        this.mostrarNotas(this.notas);
         intanceModalNotas.hide();
     }
     seleccionarNota(index){
@@ -70,12 +71,41 @@ class Notas{
         this.notas.splice(index, 1);        
         this.guardarNota();
     }
+    buscarNota(event){
+        const textoBuscar=event.target.value.trim();
+        const notasFiltradas=this.notas.filter(nota=>{
+            return nota.titulo.toLowerCase().includes(textoBuscar.toLowerCase()) ||
+            nota.texto.toLowerCase().includes(textoBuscar.toLowerCase());
+        });
+        if(notasFiltradas.length>=1){
+            contenedorAnuncios.innerHTML=`
+                <div class="col-12 d-flex justify-content-center mt-3 mb-4" data-bs-theme="dark">
+                    <button type="button" class="me-1 rounded-circle border p-2 btn-close" id="cancelarBusqueda" onclick="notas.cancelarBusqueda()"></button>
+                </div>
+            `;
+        }else{
+            contenedorAnuncios.innerHTML=`
+                <div class="col-12 d-flex justify-content-center">
+                    <h1 class="text-center opacity-50">
+                        Sin Resultados
+                    </h1>
+                </div>
+                <div class="col-12 d-flex justify-content-center mt-3 mb-4" data-bs-theme="dark">
+                    <button type="button" class="me-1 rounded-circle border p-2 btn-close" id="cancelarBusqueda" onclick="notas.cancelarBusqueda()"></button>
+                </div>`
+        }
+        this.mostrarNotas(notasFiltradas);
+    }
 
-
+    cancelarBusqueda(){
+        document.getElementById('buscar').value='';
+        contenedorAnuncios.innerHTML=''
+        this.mostrarNotas(this.notas);
+    }
 }
 
 const notas=new Notas();
-notas.mostrarNotas();
+notas.mostrarNotas(notas.notas);
 
 ModalNotas.addEventListener('hidden.bs.modal',()=>{
     document.getElementById('tituloNotas').value='';
