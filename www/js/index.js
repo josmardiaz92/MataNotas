242a1cfe95a43e290b9c512fbdb9c17be7497f40
josmardiaz92 <josmardiaz92@gmail.com>
2024-10-27 18:19:40 -0400

    Clase 3 - editar y eliminar listo

--- www/js/index.js
+++ www/js/index.js
@@ -13,10 +13,10 @@ class Notas{
     }
     mostrarNotas(){
         contenedorNotas.innerHTML='';
-        this.notas.forEach(nota => {
+        this.notas.forEach((nota,index) => {
             contenedorNotas.innerHTML+=`
                 <div class="col">
-                    <div class="card">
+                    <div class="card" onclick="notas.seleccionarNota(${index})" id="${index}">
                         <div class="card-body">
                             <h5 class="card-title">${nota.titulo}</h5>
                             <p class="card-text">${nota.texto}</p>
@@ -38,6 +38,39 @@ class Notas{
         this.mostrarNotas();
         intanceModalNotas.hide();
     }
+    seleccionarNota(index){
+        const btnEditar=document.getElementById('btnEditar');
+        const btnEliminar=document.getElementById('btnEliminar');
+        document.getElementById('btnAgregar').hidden=true;
+        btnEditar.hidden=false;
+        btnEliminar.hidden=false;
+        const nota = this.notas[index];
+        const titulo=document.getElementById('tituloNotas');
+        const texto=document.getElementById('textoNotas');
+        titulo.value=nota.titulo;
+        texto.value=nota.texto;
+
+        intanceModalNotas.show();
+
+        btnEditar.onclick=()=>{
+            this.editarNota(index);
+        };
+        btnEliminar.onclick=()=>{
+            this.eliminarNota(index);
+        };
+    }
+
+    editarNota(index){
+        const titulo=document.getElementById('tituloNotas').value;
+        const texto=document.getElementById('textoNotas').value;
+        this.notas[index]=({ titulo, texto });
+        this.guardarNota();
+    }
+    eliminarNota(index){
+        this.notas.splice(index, 1);        
+        this.guardarNota();
+    }
+
 
 }
 
@@ -47,4 +80,7 @@ notas.mostrarNotas();
 ModalNotas.addEventListener('hidden.bs.modal',()=>{
     document.getElementById('tituloNotas').value='';
     document.getElementById('textoNotas').value='';
+    document.getElementById('btnEditar').hidden=true;
+    document.getElementById('btnEliminar').hidden=true;
+    document.getElementById('btnAgregar').hidden=false;
 })
